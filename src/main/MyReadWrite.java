10ddc0e2b9aaf8685c6522ecccd631ff88978f2a
pbfsf16ago <pbfsf16ago@pb.bib.de>
2017-04-05 13:57:33 +0200

    First Commit
    First Commit of Project jAD_Editor

---
+++ src/main/MyReadWrite.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.util.ArrayList;
+
+//import java.util.
+
+public class MyReadWrite 
+{
+	
+	//Profile aus dem Ordner auslesen
+	public static String[][] getProfiles()
+	{
+		String[][] profilListe = new String[1][1];
+		return profilListe;
+	}
+	
+	//Auslesen einer Datei mit übergebenen Pfad
+	public static ArrayList readFile(String path)
+	{
+		ArrayList dateiInhalt = null;
+		return dateiInhalt;
+	}
+	
+	//Schreiben eines Inhalts in eine Datei
+	public static void writeFile(String path, ArrayList content)
+	{
+		
+	}
+	
+	//Formatieren von Profil- und Nutzerdaten zu einer ArrayList für CSV
+	public static ArrayList formatUser(Profil activeProfile, ArrayList aLUser)
+	{
+		ArrayList dateiInhalt = null;
+		return dateiInhalt;
+	}
+	
+}

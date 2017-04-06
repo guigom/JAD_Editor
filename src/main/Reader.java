ffddf617fbcf964f9feb3e703176a338c91821db
pbfsf16agr <pbfsf16agr@pb.bib.de>
2017-04-06 14:48:02 +0200

    Reader-getProfiles
    getProfiles
    Pfad ./prof/
    Prüfung auf .prof Dateien
    Rückgabe ArrayList mit den Pfaden der .prof Dateien

--- src/main/Reader.java
+++ src/main/Reader.java
@@ -1,15 +1,30 @@
 package main;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Reader 
 {
 
 	//Profile aus dem Ordner auslesen
-		public static String[][] getProfiles()
+		public static ArrayList<String> getProfiles()
 		{
-			String[][] profilListe = new String[1][1];
-			return profilListe;
+			File f = new File(".\\prof");
+			System.out.println("tostring" + f.toString());
+			ArrayList<String> al = new ArrayList<String>();
+			String comp;
+			for(File file: f.listFiles())
+			{	
+				comp = file.getPath();
+				if(comp.substring(comp.length()-5).equalsIgnoreCase(".prof"))
+				{
+				
+					al.add(comp);
+				}
+				System.out.println(comp.substring(comp.length()-5));
+			}
+			//System.out.println(al.toString());
+			return al;
 		}
 		
 		//Auslesen einer Datei mit übergebenen Pfad

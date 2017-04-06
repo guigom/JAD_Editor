e1491191e5ec7d5b1ed41cd741cb9a7380fbe8dd
pbfsf16ahe <pbfsf16ahe@pb.bib.de>
2017-04-06 12:45:42 +0200

    Update FormGroups und Profil-Testdatei
    ^this

--- src/main/FormGroups.java
+++ src/main/FormGroups.java
@@ -35,7 +35,7 @@ public class FormGroups extends JFrame {
 	 * Create the frame.
 	 */
 	public FormGroups() {
-		setTitle("Gruppen");
+		setTitle("Gruppen verwalten");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 386, 512);
 		frm_groups = new JPanel();

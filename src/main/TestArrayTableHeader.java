9cf3db4fcaf04fe24762d265837e65c34f587231
pbfsf16ahe <pbfsf16ahe@pb.bib.de>
2017-04-07 15:03:12 +0200

    TableHeader Testklasse update

--- src/main/TestArrayTableHeader.java
+++ src/main/TestArrayTableHeader.java
@@ -3,7 +3,7 @@ package main;
 public class TestArrayTableHeader {
 	public static String[] getTestStringHeaderArray()
 	{
-		//test
+		//test wffefef
 		String[] data = 
 			    {"Col1", "Col2","Col3"};
 		return data;

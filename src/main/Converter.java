c5b75449556ae982866542d4cc17e7486fc86eac
pbfsf16aja <pbfsf16aja@pb.bib.de>
2017-04-07 15:03:25 +0200

    Converter WIP
    - Converter bearbeitet, noch WIP
    - Beispiel CSV hinzugefügt

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -9,21 +9,27 @@ public class Converter
 	public static ArrayList<String> formatUser(Profil activeProfile)
 	{
 		//Variablen initalisieren
-		ArrayList<String> dateiInhalt = new ArrayList<>();
-		
+		ArrayList<String> 	dateiInhalt = new ArrayList<>();
+		String				zeile		= "";
+		int					counter		= 0;
 		
 		//Erste Zeile
-		dateiInhalt.add("#CSV-Header");
+		dateiInhalt.add("#TYPE Microsoft.ActiveDirectory.Management.ADUser");
 		
 		//Zweite Zeile, Attribute des Profils
+		//TODO: Anzeigenamen abschneiden
 		for(String attr: activeProfile.getProfGenInfo())
 		{
-			
+			zeile+=("\""+attr+"\"");
+			if(++counter!=activeProfile.getProfGenInfo().size())
+				zeile+=",";
 		}
+		//TODO: Einträge für Gruppen und OU erstellen
+		dateiInhalt.add(zeile);
+		zeile="";
 		
-		
-		
-		
+		//Folgende Zeilen für User Einträge
+		//TODO: User Einträge erstellen
 		
 		
 		return dateiInhalt;

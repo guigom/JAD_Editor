4c5f55a42c915217669f1764db0eebbca793cc46
pbfsf16ahe <pbfsf16ahe@pb.bib.de>
2017-04-07 14:40:15 +0200

    Test

---
+++ src/main/TestArrayTableHeader.java
@@ -0,0 +1,12 @@
+package main;
+
+public class TestArrayTableHeader {
+	public static String[] getTestStringHeaderArray()
+	{
+		//test
+		String[] data = 
+			    {"Col1", "Col2","Col3"};
+		return data;
+		
+		}
+}
---
+++ src/main/TestTableContent.java
@@ -0,0 +1,21 @@
+package main;
+
+public class TestTableContent {
+public static String[][] getTestStringArray()
+{
+	String[][] data = {
+		    {"Kathy", "Smith",
+		     "Snowboarding"},
+		    {"John", "Doe",
+		     "Rowing"},
+		    {"Sue", "Black",
+		     "Knitting"},
+		    {"Jane", "White",
+		     "Speed reading"},
+		    {"Joe", "Brown",
+		     "Pool"}
+		};
+	return data;
+	
+	}
+}

6512c9ecf77c7cc6a8c85895c47641662580e113
pbfsf16aja <pbfsf16aja@pb.bib.de>
2017-04-18 11:31:47 +0200

    Converter, TestConverter
    Converter und TestConverter hinzugefügt

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -17,19 +17,34 @@ public class Converter
 		dateiInhalt.add("#TYPE Microsoft.ActiveDirectory.Management.ADUser");
 		
 		//Zweite Zeile, Attribute des Profils
-		//TODO: Anzeigenamen abschneiden
 		for(String attr: activeProfile.getProfGenInfo())
 		{
-			zeile+=("\""+attr+"\"");
-			if(++counter!=activeProfile.getProfGenInfo().size())
-				zeile+=",";
+			//Anzeigename abschneiden
+			attr=attr.substring(attr.indexOf(',')+1);
+			//Anführungszeichen+Komma hinzufügen
+			zeile+=("\""+attr+"\""+",");
 		}
 		//TODO: Einträge für Gruppen und OU erstellen
+		zeile+=("\"PrimaryGroup\",\"Path\"");
+		//Zeile schreiben
 		dateiInhalt.add(zeile);
-		zeile="";
 		
 		//Folgende Zeilen für User Einträge
 		//TODO: User Einträge erstellen
+		for(User nutzer: activeProfile.getALUSER())
+		{
+			//Zeile leeren
+			zeile="";
+			//Attribute auslesen
+			for(String attr: nutzer.getUserGenInfo())
+			{
+				zeile+=("\""+attr+"\",");
+			}
+			//Guppe und OU anfügen
+			zeile+=("\""+nutzer.getUserGroup()+"\",\""+nutzer.getUserOU()+"\"");
+			//Zeile schreiben
+			dateiInhalt.add(zeile);
+		}
 		
 		
 		return dateiInhalt;
--- src/main/TestConverter.java
+++ src/main/TestConverter.java
@@ -9,7 +9,17 @@ public class TestConverter {
 	{
 		try 
 		{
-			Profil P1 = new Profil(".//prof//Profil1.prof");
+			Profil P1 				= new Profil(".//prof//Profil1.prof");
+			ArrayList<String> A1 	= new ArrayList<>();
+			A1.add("Mustermann");
+			A1.add("Max");
+			A1.add("MMustermann");
+			A1.add("Test-Eintrag");
+			
+			User U1					= new User(A1,"CN=Domänen-Benutzer,CN=Users,DC=bib,DC=loc","OU=Abteilung 1,OU=Firma,DC=bib,DC=loc");
+			
+			P1.addALUser(U1);
+			
 			for (String x: Converter.formatUser(P1))
 			{
 				System.out.println(x);

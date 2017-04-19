b28a8f507e05fef68a2fe38231dd16477b28ed00
pbfsf16ahe <pbfsf16ahe@pb.bib.de>
2017-04-19 11:08:16 +0200

    Upload für Johannes

--- src/main/FormGroups.java
+++ src/main/FormGroups.java
@@ -9,32 +9,35 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JList;
 import javax.swing.border.BevelBorder;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.AbstractListModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class FormGroups extends JFrame {
+public class FormGroups extends JDialog {
 
 	private JPanel frm_groups;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FormGroups frame = new FormGroups();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					FormGroups frame = new FormGroups(TestProfil.getTestProfil());
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
 	 */
-	public FormGroups() {
+	public FormGroups(Profil p, User u) {
 		setTitle("Gruppen verwalten");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 386, 512);
@@ -42,22 +45,24 @@ public class FormGroups extends JFrame {
 		frm_groups.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(frm_groups);
 		frm_groups.setLayout(null);
+		System.out.println(p.getProfGroups().toString());
 		
-		JList lst_groups = new JList();
-		lst_groups.setModel(new AbstractListModel() {
-			String[] values = new String[] {};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
+		JList lst_groups = new JList(p.getProfGroups().toArray());
+		
 		lst_groups.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		lst_groups.setBounds(12, 13, 344, 394);
 		frm_groups.add(lst_groups);
 		
 		JButton btn_ok = new JButton("OK");
+		btn_ok.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String selected = (String) lst_groups.getSelectedValue();
+				System.out.println(Converter.getStringBack(selected));
+				u.setUserGroup("Hallo");
+
+			}
+		});
 		btn_ok.setBounds(260, 420, 96, 32);
 		frm_groups.add(btn_ok);
 	}

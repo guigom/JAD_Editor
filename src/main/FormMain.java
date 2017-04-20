1e36e0328bf421f2e93028f23316f90526d5643f
pbfsf16ago <pbfsf16ago@pb.bib.de>
2017-04-20 14:44:41 +0200

    letzter_im_bib_Donnertag

--- src/main/FormMain.java
+++ src/main/FormMain.java
@@ -4,11 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import javax.swing.JMenuItem;
-=======
+//=======
 import javax.swing.JLabel;
->>>>>>> refs/remotes/origin/develop_model
+//>>>>>>> refs/remotes/origin/develop_model
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.border.EmptyBorder;
@@ -16,7 +16,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 import javax.swing.JButton;
-import javax.swing.AbstractAction;
+//import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
@@ -31,7 +31,7 @@ import java.util.ArrayList;
 import javax.swing.JScrollPane;
 
 
-import main.PopupMenu.MyMouseAdapter;
+//import main.PopupMenu.MyMouseAdapter;
 
 public class FormMain extends JFrame {
 
@@ -170,52 +170,52 @@ public class FormMain extends JFrame {
 		
 //>>>>>>> refs/remotes/origin/develop_model
 		scp_main.setViewportView(tbl_main);
-<<<<<<< HEAD
-		
-/** Kontextmenü
-// TODO:  Das ganze Menu kontextabhängig auslagern in
-// eine eigene Klasse. 
 
-// was kommt rein? 
-// Copy 
-// Paste */
+		/** Kontextmenü
+		 TODO:  Das ganze Menu kontextabhängig auslagern in
+		 eine eigene Klasse. 
+
+		 was kommt rein? 
+		 Copy 
+		 Paste */
 
 
-// erzeuge Kontextmenü 		
+		// erzeuge Kontextmenü 		
 		JPopupMenu context	= new JPopupMenu("Context");
-		
-// erzeuge Menüeintrag
+
+		// erzeuge Menüeintrag
 		// hat keinen verbundenen Listener
 		context.add( new JMenuItem("Erster Eintrag ") );
 		context.addSeparator();
-// erzeuge Menüeintrag
+		// erzeuge Menüeintrag
 		JMenuItem	cCopy	= new JMenuItem("Copy");
 		context.add(cCopy);
-// 
-		
+		// 
+
 		cCopy.addActionListener( e-> {
 			System.out.printf("Ausgabe cCopy %s%n", e.getActionCommand() );
 			// TODO: wen rufe ich hier auf?
 			//tbl_main.getSelectedColumns(); 
 		});
-// erzeuge weitere, zufällige Menüeinträge 
-	    for ( String s : ("das sind dummy einräge," +
-                "zweiter Eintrag," +
-                "dritter eintrag, vierter Eintrag").split(",") )
+		// erzeuge weitere, zufällige Menüeinträge 
+		for ( String s : 
+			("das sind dummy einräge," +
+				"zweiter Eintrag," +
+				"dritter eintrag, vierter Eintrag").split(",") )
 		{
-//		 context.add( new AbstractAction(s) {
-//		   @Override public void actionPerformed( ActionEvent e ) {
-////		     tbl_main.addRowSelectionInterval(1, 3) ;
-//			 System.out.println( tbl_main.getColumnName(tbl_main.getSelectedColumn()) );  
-//		     System.out.println("FormMain.FormMain(...).new AbstractAction() {...}.actionPerformed()");
-					//.append( e.getActionCommand() + "\n" );
-//		   }
-//		 } );
+			//				 context.add( new AbstractAction(s) {
+			//				   @Override public void actionPerformed( ActionEvent e ) {
+			////				     tbl_main.addRowSelectionInterval(1, 3) ;
+			//					 System.out.println( tbl_main.getColumnName(tbl_main.getSelectedColumn()) );  
+			//				     System.out.println("FormMain.FormMain(...).new AbstractAction() {...}.actionPerformed()");
+			//.append( e.getActionCommand() + "\n" );
+			//				   }
+			//				 } );
 		}
 		// füge das Kontextmenü der Tabelle hinzu.
-//		tbl_main.add(context);
-//		scp_main.add(context);
-		
+		//				tbl_main.add(context);
+		//				scp_main.add(context);
+
 		// das MouseEvent informiert den Controller, dass 
 		// es gedrückt wurde: isPopupTrigger()
 		// 
@@ -230,25 +230,21 @@ public class FormMain extends JFrame {
 			}
 			public void mousePressed(MouseEvent e){
 				if ( e.getButton()==MouseEvent.BUTTON1 ){
-//					System.out.printf("MouseEvent: %h %s%n", e.getSource(),	((JTable)e.getSource()).getTableHeader());
-					 System.out.println( tbl_main.getColumnName(tbl_main.getSelectedColumn()) );
-				     System.out.println("FormMain.FormMain(...).new AbstractAction() {...}.actionPerformed()");
+					//							System.out.printf("MouseEvent: %h %s%n", e.getSource(),	((JTable)e.getSource()).getTableHeader());
+					System.out.println( tbl_main.getColumnName(tbl_main.getSelectedColumn()) );
+					System.out.println("FormMain.FormMain(...).new AbstractAction() {...}.actionPerformed()");
 				}
-//				if ( e.getButton()==MouseEvent.BUTTON3 ){
-//					Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
-//					Transferable trans = cb.getContents(null);
-//					System.out.printf("Clipboard beinhaltet einen String: %b %n",
-//							trans.isDataFlavorSupported(DataFlavor.stringFlavor) );
-				}
+				//						if ( e.getButton()==MouseEvent.BUTTON3 ){
+				//							Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+				//							Transferable trans = cb.getContents(null);
+				//							System.out.printf("Clipboard beinhaltet einen String: %b %n",
+				//									trans.isDataFlavorSupported(DataFlavor.stringFlavor) );
+			}
 		} // MouseAdapter
 		MyMouseAdapter ma = new MyMouseAdapter(); 
 		tbl_main.addMouseListener(ma);
 		scp_main.addMouseListener(ma);
-//		setComponentPopupMenu(context);
-
-	} // FormMain()
-}// FormMain
-=======
+		//				setComponentPopupMenu(context);
 	}
 	
 	public void alter_table()
@@ -268,11 +264,10 @@ public class FormMain extends JFrame {
 		ArrayList<String> col_names = new ArrayList<String>(activeProfile.getProfGenInfo());
 		col_names.add("Gruppe");	//fixe Werte
 		col_names.add("OU");		
-		
+
 		tbl_main = new JTable(row_data, col_names.toArray());
 		scp_main.setViewportView(tbl_main);
 		System.out.println("Tabelleninhalt: " + tbl_main.getModel().getValueAt(0, 0));
-
 	}
-}
->>>>>>> refs/remotes/origin/develop_model
+} // FormMain()
+

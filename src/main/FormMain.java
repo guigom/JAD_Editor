5b6bd677da03c1821c8d172991545e1cb188fb20
pbfsf16ago <pbfsf16ago@pb.bib.de>
2017-04-20 09:25:06 +0200

    Kontextmenü in Hauptfenster
    Kontextmenü sichtbar.

--- src/main/FormMain.java
+++ src/main/FormMain.java
@@ -9,10 +9,14 @@ import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
@@ -127,18 +131,54 @@ public class FormMain extends JFrame {
 		scp_main.setBounds(12, 16, 780, 704);
 		frm_main.add(scp_main);
 		
-		tbl_main = new JTable(TestTableContent.getTestStringArray(),TestArrayTableHeader.getTestStringHeaderArray());
+		tbl_main = new JTable(TestTableContent.getTestStringArray(),
+				TestArrayTableHeader.getTestStringHeaderArray());
 		scp_main.setViewportView(tbl_main);
 		
 		// Kontextmenü
+		// TODO: 
+		// Das ganze Menu kontextabhängig auslagern in
+		// eine eigene Klasse. 
 		JPopupMenu context	= new JPopupMenu("Context");
+//		add
+		
 		JMenuItem		cCopy		= new JMenuItem("Copy");
 		context.add(cCopy);
 		cCopy.addActionListener( e-> {
 			System.out.printf("öffne FormAdd %s%n", e.getActionCommand() );
 			// TODO: wen rufe ich hier auf?
+			tbl_main.getSelectedColumns(); 
 		});
 		
-		setComponentPopupMenu(context);
-	}
-}
+		// füge das Kontextmenü der Tabelle hinzu.
+		tbl_main.add(context);
+		
+		// das MouseEvent informiert den Controller, das 
+		// es gedrückt wurde
+		// 
+		class MyMouseAdapter extends MouseAdapter {
+			/**
+			 * wenn der Popup Trigger gedrückt wird <br>
+			 * dann untersuche das Feld, und öffne dort das Menü
+			 */
+			public void mouseReleased(MouseEvent e){
+				if ( e.isPopupTrigger())
+					context.show( e.getComponent(), e.getX(), e.getY() );
+			}
+			public void mousePressed(MouseEvent e){
+//				if ( e.getButton()==MouseEvent.BUTTON1 ){
+//					System.out.printf("MouseEvent: %h %s%n", e.getSource(),
+//							((JTable)e.getSource()).getTableHeader());
+//				}
+//				if ( e.getButton()==MouseEvent.BUTTON3 ){
+//					Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+//					Transferable trans = cb.getContents(null);
+//					System.out.printf("Clipboard beinhaltet einen String: %b %n",
+//							trans.isDataFlavorSupported(DataFlavor.stringFlavor) );
+				}
+		} // MouseAdapter
+		MyMouseAdapter ma = new MyMouseAdapter(); 
+		tbl_main.addMouseListener(ma);
+//		setComponentPopupMenu(context);
+	} // FormMain()
+}// FormMain
